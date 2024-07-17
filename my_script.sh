fb9d71c049dfbaa2d096959c58876db025dc09d4
danHavercroft <32074604+danHavercroft@users.noreply.github.com>
2024-07-17 14:50:03 +0100

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -5,3 +5,5 @@ echo "Running automation..." > ./output_log.txt
 echo "" >> ./output_log.txt
 echo "Checking uptime..." >> ./output_log.txt
 echo $(uptime) >> ./output_log.txt
+
+echo "THIS IS MY UPDATED SCRIPT" >> ./output_log.txt
